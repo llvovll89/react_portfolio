5ea8baf84add6de61f71b3aeba2d13424fd59ef7
GeonHo Kim <svvvs5579@naver.com>
2025-07-27 17:29:54 +0900

    chore: save

--- src/common/toggle/DarkMode.jsx
+++ src/common/toggle/DarkMode.jsx
@@ -1,50 +1,50 @@
-import React, { useContext } from 'react';
-import { CiDark, CiLight } from 'react-icons/ci';
-import { DarkContext } from '../../context/darkContext';
-import './DarkMode.css'
+import React, {useContext} from "react";
+import {CiDark, CiLight} from "react-icons/ci";
+import {DarkContext} from "../../context/darkContext";
+import "./DarkMode.css";
 
 export const DarkMode = () => {
-  const { darkMode, setDarkMode } = useContext(DarkContext);
+    const {darkMode, setDarkMode} = useContext(DarkContext);
 
-  const handleClick = () => {
-    setDarkMode(!darkMode);
-  };
+    const handleClick = () => {
+        setDarkMode(!darkMode);
+    };
 
-  const containerStyles = {
-    background: darkMode ? '#231f20' : '#FFF',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    gap: '2.5px',
-    boxShadow: '0px 1px 2px rgba(0,0,0,0.16)',
-  };
+    const containerStyles = {
+        background: darkMode ? "#231f20" : "#FFF",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        gap: "2.5px",
+        boxShadow: "0px 1px 2px rgba(0,0,0,0.16)",
+    };
 
-  const iconStyles = {
-    color: darkMode ? '#FFFFFF' : '#231f20',
-  };
+    const iconStyles = {
+        color: darkMode ? "#FFFFFF" : "#231f20",
+    };
 
-  const textStyles = {
-    color: darkMode ? '#FFFFFF' : '#231f20',
-    fontWeight: 'bold',
-  };
+    const textStyles = {
+        color: darkMode ? "#FFFFFF" : "#231f20",
+        fontWeight: "bold",
+    };
 
-  return (
-    <div
-      className="dark_container"
-      onClick={handleClick}
-      style={containerStyles}
-    >
-      {darkMode ? (
-        <>
-          <CiDark style={iconStyles} />
-          <span style={textStyles}>Dark</span>
-        </>
-      ) : (
-        <>
-          <CiLight style={iconStyles} />
-          <span style={textStyles}>Light</span>
-        </>
-      )}
-    </div>
-  );
+    return (
+        <div
+            className="dark_container"
+            onClick={handleClick}
+            style={containerStyles}
+        >
+            {darkMode ? (
+                <>
+                    <CiDark style={iconStyles} />
+                    <span style={textStyles}>Dark</span>
+                </>
+            ) : (
+                <>
+                    <CiLight style={iconStyles} />
+                    <span style={textStyles}>Light</span>
+                </>
+            )}
+        </div>
+    );
 };
